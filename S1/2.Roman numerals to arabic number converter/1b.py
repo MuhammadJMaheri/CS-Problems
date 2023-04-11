fd2d0571c15ee9424a7380a09cf597efa1ae7e05
MuhammadJMaheri <111657852+MuhammadJMaheri@users.noreply.github.com>
2023-04-11 21:51:38 +0330

    Update 1b.py
    made it PEP8 compatible

--- S1/2.Roman numerals to arabic number converter/1b.py
+++ S1/2.Roman numerals to arabic number converter/1b.py
@@ -1,23 +1,37 @@
+'''contains roman numerals and their equavilant arabic numbers'''
 num={'V':5,'L':50,'D':500,'I':1,'X':10,'C':100,'M':1000}
+
 rom = input("enter roman numerals: ")
-def findmax(string,a,b):
-    p=0
-    for i in range(a,b+1):
-        if num[string[i]]>p:
-            p=num[string[i]]
+
+def findmax(string,left_index,right_index):
+    '''
+    finds the smallest index that represents the 
+    greatest numeral from left_index to right_index
+    '''
+    max_decimal=0
+    for i in range(left_index,right_index+1):
+        if num[string[i]]>max_decimal:
+            max_decimal=num[string[i]]
             pos=i
     return pos
-c=0
-def calc(string,a,b):
-    if(string==''or a>b):
+
+def calc(string,left_index,right_index):
+    '''
+    calculates arabic equavilant from left_index to right_index
+    '''
+    if(string==''or left_index>right_index):
         return 0
-    fm=findmax(string,a,b)
-    if(a==b):
-        c=num[rom[a]]
-        return c
-    else:
-        c=num[rom[fm]]+calc(string,fm+1,b)-calc(string,a,fm-1)
-        return c
+
+    max_index=findmax(string,left_index,right_index)
+    if left_index==right_index:
+        decimal=num[rom[left_index]]
+        return decimal
+
+    decimal=  (num[rom[max_index]]
+        +calc(string,max_index+1,right_index)
+        -calc(string,left_index,max_index-1))
+    return decimal
+
 arabic=calc(rom,0,len(rom)-1)
 print("arabic number: ")
 print(arabic)
