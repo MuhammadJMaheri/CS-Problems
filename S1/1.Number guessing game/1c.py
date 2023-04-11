0ee1b20038bf5b5338d89755517ff0fa1d5340db
MuhammadJMaheri <111657852+MuhammadJMaheri@users.noreply.github.com>
2023-04-11 21:50:05 +0330

    Update 1c.py
    made it PEP8 compatible

--- S1/1.Number guessing game/1c.py
+++ S1/1.Number guessing game/1c.py
@@ -1,13 +1,17 @@
+'''module for generating random variables'''
 import random
+
 n=random.randrange(0,100)
 i=0
+
 while i<10:
     guess=int(input("enter your guess: "))
     i=i+1
+
     if guess==n :
         print("equal")
         break
-    elif guess<n :
+    if guess<n :
         print("n is bigger")
     else :
         print("n is smaller")
